82603284399aedeff1bd58b6a5ce3bacc1ff8ceb
43695512 <yanter@wp.pl>
2021-11-10 12:33:52 +0100

    Small changes + ScrollToTop

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Projectss from "../components/Projects/Projects";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Projects = () => {
   return (
     <>
+      <ScrollToTop />
       <Projectss />
     </>
   );
