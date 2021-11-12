922b4612246f462bd9b16b8cc4c5922f4682a91a
43695512 <yanter@wp.pl>
2021-11-12 22:46:55 +0100

    Responsive Projects Element

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -31,68 +31,70 @@ const Projects = () => {
       <br />
       <br />
       <br />
-      <Icon style={{ marginTop: "200px", marginBottom: "200px" }} to="/">
-        Summary
-      </Icon>
-      <br />
-      <br />
-      <div
-        style={{
-          display: "flex",
-          width: "80vw",
-          margin: "auto",
-          alignContent: "center",
-          justifyContent: "center",
-        }}
-      >
-        <table>
-          <thead>
-            <tr>
-              <th>Number</th>
-              <th>Title</th>
-              <th>Time Spent</th>
-              <th>Technologies</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>YelpCamp</td>
-              <td>~100h</td>
-              <td>Node JS | DataBase</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>My Hobby</td>
-              <td>~70h</td>
-              <td>React JS | Jquery</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>CryptoCurrency</td>
-              <td>~10h</td>
-              <td>React JS | Redux | RapidAPI</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>Messenger App</td>
-              <td>~8h</td>
-              <td>React JS | Firebase | Chat Engine</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>Paralax Scrolling</td>
-              <td>~5h</td>
-              <td>HTML | CSS | JS</td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>Museum Of Candy</td>
-              <td>~2h</td>
-              <td>HTML | CSS</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="gototable">
+        <Icon style={{ marginTop: "200px", marginBottom: "200px" }} to="/">
+          Summary
+        </Icon>
+        <br />
+        <br />
+        <div
+          style={{
+            display: "flex",
+            width: "80vw",
+            margin: "auto",
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+        >
+          <table>
+            <thead>
+              <tr>
+                <th>Number</th>
+                <th>Title</th>
+                <th>Time Spent</th>
+                <th>Technologies</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>YelpCamp</td>
+                <td>~100h</td>
+                <td>Node JS | DataBase</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>My Hobby</td>
+                <td>~70h</td>
+                <td>React JS | Jquery</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>CryptoCurrency</td>
+                <td>~10h</td>
+                <td>React JS | Redux | RapidAPI</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Messenger App</td>
+                <td>~8h</td>
+                <td>React JS | Firebase | Chat Engine</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>Paralax Scrolling</td>
+                <td>~5h</td>
+                <td>HTML | CSS | JS</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>Museum Of Candy</td>
+                <td>~2h</td>
+                <td>HTML | CSS</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <br />
       <br /> <br />
