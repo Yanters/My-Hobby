0b968f3f0221c3f3b72b197d1693b8c5947f22bf
43695512 <yanter@wp.pl>
2021-11-25 21:37:26 +0100

    Small CSS replacement

--- src/components/Contact/FormSuccess.tsx
+++ src/components/Contact/FormSuccess.tsx
@@ -6,7 +6,7 @@ const FormSuccess = () => {
   return (
     <div className="form-content-right">
       <h1 className="form-success">We have received your email!</h1>
-      <img className="form-img-2" src={image6} alt="success" />
+      <img id="form-img-2" src={image6} alt="success" />
     </div>
   );
 };
